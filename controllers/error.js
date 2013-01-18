0720aa0529bbcfd832c07f0c179377ec344c0b69
Rocco Zanni <rocco.zanni@gmail.com>
2013-01-18 22:29:12 +0100

    Added email traceback

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,4 @@
+var SendGrid = require('sendgrid').SendGrid;
 
 module.exports = function(app) {
 
@@ -10,10 +11,24 @@ module.exports = function(app) {
     // Error Handler
     app.use(function(err, req, res, next){
 
-        console.error(err.stack);
-
+        // Render page
         res.status(500);
         res.render('error/500');
-    });
 
+        // Log on console
+        console.error(err.stack);
+
+        if (!process.env.SENDGRID_USERNAME || !process.env.SENDGRID_PASSWORD) {
+            return;
+        }
+
+        // Send debug email
+        new SendGrid(process.env.SENDGRID_USERNAME,process.env.SENDGRID_PASSWORD).send({
+          to:       process.env.DEBUG_MAIL_TO,
+          from:     process.env.DEBUG_MAIL_FROM,
+          subject:  'Nodepad exception',
+          text:     err.stack
+        });
+
+    });
 };
